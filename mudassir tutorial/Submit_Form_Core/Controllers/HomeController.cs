d85d2201c757df59dd50ee359dc0ff03cf13ccd6
Chesters-CZ <62833819+Chesters-CZ@users.noreply.github.com>
2023-01-07 02:10:44 +0100

    bych řek, že hotojo

---
+++ mudassir tutorial/Submit_Form_Core/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Submit_Form_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string firstName, string lastName)
+        {
+            ViewBag.Name = string.Format("Name: {0} {1}", firstName, lastName);
+            return View();
+        }
+    }
+}
